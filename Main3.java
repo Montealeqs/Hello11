186635f23d9b61e40e34cdf74de95a7da73ef5ac
Montealeqs <81868901+Montealeqs@users.noreply.github.com>
2021-10-25 01:00:39 -0800

    3 git commit

---
+++ Main3.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        8. Տպել տրված n թվից մինչեւ 0 հետ հաշվարկով։
+        int n = 10;
+        for (int i = n; i > 0; i--)
+            System.out.print(i + " ");
+    }
+}
+
