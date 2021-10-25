1408f58cd0cd622553a12b0e454d64cbf9b458cc
Montealeqs <81868901+Montealeqs@users.noreply.github.com>
2021-10-25 12:02:54 -0800

    7 git commit

---
+++ Main7.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+      int sum=0;
+        int arr[]=new int[sum] ;
+        for (int i = -100;i<=0 ; i++){
+            sum++;
+            System.out.println(i);
+        }
+    }
+}
