45af1b09fb524910833b9d3c2dbd715aef4ee3f7
Montealeqs <81868901+Montealeqs@users.noreply.github.com>
2021-10-25 00:41:19 -0800

    first git commit

--- Main.java
+++ Main.java
@@ -3,24 +3,35 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-//        3. Կազմել ծրագիր, որ կտպի 3 թվերից փոքրագույնը:
-        int a = 40;
-        int b = 20;
-        int c = 30;
-        int poqraguyn;
-        if (a < b) {
-            if (c < a) {
-                poqraguyn = c;
-            } else {
-                poqraguyn = a;
-            }
-        } else {
-            if (b < c) {
-                poqraguyn = b;
-            } else {
-                poqraguyn = c;
-            }
-            System.out.println(poqraguyn);
+//        5. Վերցնել ամբողջ թիվ: Switch օպերատորի միջոցով տպել
+//        շաբաթվա օրը,
+//        որը համապատասխանում է մեր թվին: Եթե այդպիսի շաբաթվա օր
+//        գոյություն չունի՝ տպել "Գոյություն չունի":
+        int day = 4;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Գոյություն չունի");
         }
     }
 }
