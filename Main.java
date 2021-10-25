d20f11888e964ab0d3d27d18402457679a7f8a25
Montealeqs <81868901+Montealeqs@users.noreply.github.com>
2021-10-25 00:45:32 -0800

    first 2 git commit

--- Main.java
+++ Main.java
@@ -3,35 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-//        5. Վերցնել ամբողջ թիվ: Switch օպերատորի միջոցով տպել
-//        շաբաթվա օրը,
-//        որը համապատասխանում է մեր թվին: Եթե այդպիսի շաբաթվա օր
-//        գոյություն չունի՝ տպել "Գոյություն չունի":
-        int day = 4;
-        switch (day) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
-            default:
-                System.out.println("Գոյություն չունի");
+//        Գտնել եւ տպել 0-ից մինչեւ տրված n-թվերի միջակայքում գտնվող
+//        բոլոր այն թվերը որոնք 3 եւ 5 են բաժանվում առանց մնացորդի։
+        int n=100;
+        for (int i = 0; i <n ; i++) {
+            if (i%3==0 && i%5==0)
+                System.out.println(i);
+
         }
     }
 }
