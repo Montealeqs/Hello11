2b0b7147f679bf3f0098d78d5acb3beb294b69b0
Montealeqs <81868901+Montealeqs@users.noreply.github.com>
2021-10-25 17:10:58 -0800

    2.8 git commit

--- Main2.8.java
+++ Main2.8.java
@@ -3,15 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-//        4. Գտնել եւ տպել նախադասության ամենաերկար բառը:
-        String str = "es aprum em erevanum ";
-        String[] words = str.split(" ");
-        int length = 0;
-
-        for (String word : words) {
-            if (length < word.length()) { length = word.length();
+//        Ստեղծել երկչափ զանգված 5 X 5,
+//                Լցնել ինդեքսների տարբերությունով`(i-j) եւ տպել:
+        int [][]arr =new int[5][5];
+        for (int i = 0; i <5 ; i++) {
+            for (int j = 1; j < 5; j++) {
+                arr[i][j] = i + 1;
+                System.out.print(arr[i][j] + " ");
             }
+            System.out.println();
         }
-        System.out.println(length);
     }
 }
+
